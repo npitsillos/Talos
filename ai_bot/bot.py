740a187577e0740bef5164c6b3d0b675a4e14226
Nikolas Pitsillos <npitsillos@gmail.com>
2020-02-22 20:14:36 +0000

    Created minimal robot to respond to help and contribute commands closing #2

---
+++ ai_bot/bot.py
@@ -0,0 +1,59 @@
+import discord
+import os
+import logging
+import datetime
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+from help_info import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+token = os.getenv("TALOS_TOKEN")
+
+client = discord.Client()
+bot = Bot(command_prefix="!", help_command=None)
+
+@bot.event
+async def on_ready():
+    logger.info(bot.user.name + " is online...")
+    activity = discord.Activity(name="Developing general AI! Use !help", type=discord.ActicityType.custom)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+@bot.event
+async def on_message(message):
+    if bot.user in message.mentions:
+        await message.channel.send("Μόλις απόχτησα συνείδησην... Πε μου;")
+    await bot.process_commands(message)
+
+async def send_help_page(ctx, page):
+    help_info = "--- " + page.upper() + " HELP PAGE ---\n" + help_dict[page]
+    while len(help_info) > 1900:  # Embed has a limit of 2048 chars
+        idx = help_info.index('\n', 1900)
+        emb = discord.Embed(description=help_info[:idx], colour=4387968)
+        await ctx.author.send(embed=emb)
+    emb = discord.Embed(description=help_info, colour=4387968)
+    await ctx.author.send(embed=emb)
+
+@bot.command()
+async def help(ctx, *params):
+    if isinstance(ctx.channel, discord.DMChannel):
+        if len(params) > 0:
+            for page in params:
+                if page in help_dict.keys():
+                    await send_help_page(ctx, page)
+                else:
+                    await ctx.channel.send("Έψαξα τα αρχεία μου αλλά ένηβρα έτσι πράμα. Τούτα είμαι χαρντκοουτετ να δείχνω: {}\n Artificial Intelligence μήσιημου...".format(" ".join(help_dict.keys())))
+        else:
+            for key in help_dict.keys():
+                await send_help_page(ctx, key)
+    else:
+        await ctx.channel.send("Στείλε DM ρεεε να μεν μας θωρούν ούλλοι!")
+
+def run_bot():
+    bot.run(token)
+
+if __name__ == "__main__":
+    run_bot()
---
+++ ai_bot/help_info.py
@@ -0,0 +1,12 @@
+help_dict = {}
+
+help_dict["help"] = '''
+!help [comand-list]
+Display the help message for every command
+
+!envs
+Returns a list of supported openai gym environments
+
+!contribute
+Return Github link for Talos
+'''
