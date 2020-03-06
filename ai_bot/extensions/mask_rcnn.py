410ac17b2f0e7d5d192f271915f2e030dbd7185a
Nikolas Pitsillos <npitsillos@gmail.com>
2020-03-06 23:57:49 +0000

    Updated to check if model created, closes #32, workon #28

--- ai_bot/extensions/mask_rcnn.py
+++ ai_bot/extensions/mask_rcnn.py
@@ -6,7 +6,7 @@ from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 class MaskRCNN():
 
-    def __init__(self, num_classes):
+    def __init__(self):
         self.model = self.build_model()
         self.model.eval()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -16,20 +16,20 @@ class MaskRCNN():
          # Define model and change according to our dataset
         model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
 
-        # get number of input features for the classifier
-        in_features = model.roi_heads.box_predictor.cls_score.in_features
-        # replace the pre-trained head with a new one
-        model.roi_heads.box_predictor = FastRCNNPredictor(in_features)
+        # # get number of input features for the classifier
+        # in_features = model.roi_heads.box_predictor.cls_score.in_features
+        # # replace the pre-trained head with a new one
+        # model.roi_heads.box_predictor = FastRCNNPredictor(in_features)
 
-        # now get the number of input features for the mask classifier
-        in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-        hidden_layer = 256
-        # and replace the mask predictor with a new one
-        model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer)
+        # # now get the number of input features for the mask classifier
+        # in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+        # hidden_layer = 256
+        # # and replace the mask predictor with a new one
+        # model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+        #                                                hidden_layer)
         return model
     
-    def predict_images(self, images):
+    def predict(self, images):
         predictions = []
         with torch.no_grad():
             for image in images:
